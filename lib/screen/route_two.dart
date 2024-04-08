5b6faf5137a146afbd04f804edd1296ae93d0c16
hugedreamhd <hugedreamhd@gmail.com>
2024-04-08 16:52:15 +0900

    pushAndRemoveUntil을 이용한 route 이동 및 삭제 기능

--- lib/screen/route_two.dart
+++ lib/screen/route_two.dart
@@ -48,6 +48,19 @@ class RouteTwoScreen extends StatelessWidget {
             },
             child: Text('Push Replacement'),
           ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                    builder: (_) => RouteThreeScreen(),
+                  ),
+                  (route) => route.settings.name == '/');
+              //route를 false로 하면 이동 한 후 앞 라우터스택 모두 삭제
+              //route를 true로 하면 이동 한 후 route 스택 유지
+              //route.settings.name으로 특정 라우트만 삭제 가능
+            },
+            child: Text('Push And Remove Until'),
+          ),
         ],
       ),
     );
