c9f0b35078c94e99adb1103292bde01687d48e91
hugedreamhd <hugedreamhd@gmail.com>
2024-04-08 18:22:18 +0900

    home_screen에서 maybePop 기능 테스트

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,6 +13,19 @@ class HomeScreen extends StatelessWidget {
       title: 'Home Screen',
       children: [
         ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).maybePop();
+            //라우트 스택 뒤에 스크린이 있을때만 뒤로 가게 해준다
+          },
+          child: Text('Maybe Pop'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Pop'),
+        ),
+        ElevatedButton(
           onPressed: () async {
             final result = await Navigator.of(context).push(
               MaterialPageRoute(
